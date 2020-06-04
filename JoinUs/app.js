e17b594638c22c3476b5672a5d8c4bc69d282dff
Harshit <harshit19051915@gmail.com>
2020-06-04 02:32:06 +0000

    create a form in home.ejs to send post route for inserting new user

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,14 +1,50 @@
-var express = require('express')
-var app = express()
 
-app.get("/",function(req,res){
-    res.send("This is index page")
+var express    = require('express');
+var mysql      = require('mysql');
+var ejs        = require('ejs');
+var bodyParser = require('body-parser');
+var app        = express();
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({extended:true}));
+
+var connection = mysql.createConnection({
+    host : 'localhost',
+    user : 'root',
+    password : process.env.MYSQL_PW,
+    database : 'join_us'
 });
 
 
+app.get("/",function(req,res){
+    
+    //count of users in db
+    var q = 'SELECT COUNT(*) AS total FROM users';
+    connection.query(q,function(error,results,fileds){
+        if(error)
+            throw error
+        res.render("home",{total:results[0].total});    
+    })
+});
 
+app.post("/register",function(req,res){
+    
+    //insert into db
+    var user = {
+              email: req.body.email
+    };
+    
+    var q = 'INSERT INTO users SET ? ' ;
+    connection.query(q,user,function(err,results){
+        if(err)
+            throw err
+        console.log(results)
+        res.redirect("/");            
+    });
+});
 
 
 app.listen(3000,function(req,res){
      console.log("Server started");
 })
+
