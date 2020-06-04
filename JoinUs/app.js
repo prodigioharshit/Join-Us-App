237016914340b453d25a98025514376fb7e078cd
Harshit <harshit19051915@gmail.com>
2020-06-04 00:49:02 +0000

    require express

--- JoinUs/app.js
+++ JoinUs/app.js
@@ -1,1 +1,14 @@
+var express = require('express')
+var app = express()
 
+app.get("/",function(req,res){
+    res.send("This is index page")
+});
+
+
+
+
+
+app.listen(3000,function(req,res){
+     console.log("Server started");
+})
